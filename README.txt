85dcf7c3ebddfa00766559602650574ddf2e7cf2
Leo C. Stein <leo.stein@gmail.com>
2024-02-12 22:36:27 -0600

    Copied over files from other places (no attempt to save history)

---
+++ README.txt
@@ -0,0 +1,7 @@
+The leops.el file should get symlinked to
+   ~/.emacs.d/auctex/style/leops.el
+The leops.cls file should get symlinked to
+  TEXMFHOME/tex/latex/leops/leops.cls
+where TEXMFHOME is whatever is returned from
+  kpsewhich -var-value=TEXMFHOME
+On my mac, this is /Users/leo/Library/texmf
---
+++ leops.el
@@ -0,0 +1,51 @@
+;;; leops.el --- Style hook for Leo's leops document class.  -*- lexical-binding: t; -*-
+
+;;; Code:
+
+(require 'tex)
+(require 'latex)
+
+(defvar LaTeX-leops-package-options-list
+  '(;; Uni and course info
+    ("schoolname")
+    ("deptname")
+    ("coursename")
+    ("coursenum")
+    ("termyear")
+    ("instructor")
+    ;; Problem set info
+    ("psnum")
+    ("psdue")
+    ;; Show solutions or not
+    ("solutions" ("true" "false")))
+  "Package options for the leops package.")
+
+(TeX-add-style-hook
+ "leops"
+ (function
+  (lambda ()
+    (TeX-run-style-hooks "article" "geometry" "amsmath" "amssymb"
+                         "amsfonts" "amsthm" "xcolor" "tabularx"
+                         "fontenc" "inputenc" "enumitem" "fancyhdr"
+                         "hyperref" "graphicx" "bm" "verbatim"
+                         "caption" "mdframed" "tikz" "siunitx")
+    (TeX-add-symbols
+     '("pssetup" (TeX-arg-key-val LaTeX-leops-package-options-list))
+     '("showsolutions" 0)
+     '("hidesolutions" 0)
+     '("ifsolutions" t)
+     '("ifnotsolutions" t)
+     '("makeheader" 0)
+     '("bs" t)
+     '("pd" 0)
+     '("nn" 0)
+     '("cd" 0)
+     '("avg" t)
+     '("diag" 0)
+     '("sign" 0)
+     '("Tr" 0)
+    (LaTeX-add-environments
+     '("solution" ["Label (default is 'Solution:')"]))
+    )))
+
+;;; leops.el ends here.
